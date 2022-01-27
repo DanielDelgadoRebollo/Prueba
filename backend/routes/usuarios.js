821350cce37b593636eb4d04d4c06348bd33cacf
ikanueve <72197645+ikanueve@users.noreply.github.com>
2022-01-27 14:02:29 +0100

    up

---
+++ backend/routes/usuarios.js
@@ -0,0 +1,30 @@
+const { Router } = require('express');
+const router = Router();
+
+const path = require('path');
+const { unlink } = require('fs-extra');
+
+const Usuario = require('../models/Usuario');
+
+router.get('/', async (req, res) => {
+    const usuarios = await Usuario.find().sort('-_id');
+    res.json(usuarios);
+});
+
+router.post('/', async (req, res) => {
+    const { nombre, apellidos, alias } = req.body;
+    const imagePath = '/uploads/' + req.file.filename;
+    const newUsuario = new Book({nombre, apellidos, alias, imagePath});
+    console.log(newUsuario)
+    await newUsuario.save();
+    res.json({'message': 'Usuario Saved'});
+});
+
+router.delete('/:id', async (req, res) => {
+    const usuario = await Usuario.findByIdAndDelete(req.params.id);
+    await unlink(path.resolve('./backend/public/' + usuario.imagePath));
+    res.json({message: 'Usuario Deleted'});
+});
+
+
+module.exports = router;
