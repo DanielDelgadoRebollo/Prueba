bd6d7dcf5e98194c9c561971c25831597df68d0e
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2022-02-03 13:27:02 +0100

    subid<

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect("mongodb+srv://testUser:12345@doctorstrange.wc8mn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true
 }) 
     .then(db => console.log(`DB is connected`))
